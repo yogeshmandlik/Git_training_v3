e47655ee23268d7ab617ad214bbe9263b3a2b957
yogeshmandlik <53938850+yogeshmandlik@users.noreply.github.com>
2021-12-06 23:49:27 +0530

    Create Newscript.py

---
+++ Newscript.py
@@ -0,0 +1,13 @@
+def about_me(your_name):
+    print("The wise {} loves Python.".format(your_name))            
+
+def HW():
+    print("Hello World!")
+
+def Both():
+    HW()
+    about_me("Ab")
+
+Both()
+F  = open(r"F:\Madhura\a.txt", "a")
+F.write("ok\n")
